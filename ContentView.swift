c6dcbed3f422ec32d35591d1d383d6487da1b7b0
NattyanTV <73095116+nattyan-tv@users.noreply.github.com>
2022-01-15 18:54:25 +0900

    36度台を出しやすくするメニューを追加

--- ContentView.swift
+++ ContentView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct ContentView: View {
     @State public var count = 1
     @State public var temp: [String] = [""]
+    @State public var gen_type = true
     var body: some View {
         Form {
             Section(header: Text("設定")) {
@@ -10,17 +11,33 @@ struct ContentView: View {
                     //設定できる体重の範囲を指定
                     Text("\(count)回")
                 }
+                Toggle(isOn: $gen_type) {
+                    Text("36度台を出しやすくする")
+                }
                 Button("体温生成") {
                     temp.removeAll()
                     var temp_1 = Int()
                     var temp_2 = Int()
-                    for _ in 0..<count {
-                        temp_1 = Int.random(in: 35...37)
-                        temp_2 = Int.random(in: 0...9)
-                        temp.append("\(temp_1).\(temp_2)℃")
-                    }
-                    for i in 0..<temp.count{
-                        print(temp[i])
+                    if (gen_type == true) {
+                        for _ in 0..<count {
+                            let temp_1_seed = Int.random(in: 0...20)
+                            switch temp_1_seed {
+                                case 0,1:
+                                temp_1 = 35
+                                case 19,20:
+                                temp_1 = 37
+                            default:
+                                temp_1 = 36
+                            }
+                            temp_2 = Int.random(in: 0...9)
+                            temp.append("\(temp_1).\(temp_2)℃")
+                        }
+                    }else{
+                        for _ in 0..<count {
+                            temp_1 = Int.random(in: 35...37)
+                            temp_2 = Int.random(in: 0...9)
+                            temp.append("\(temp_1).\(temp_2)℃")
+                        }
                     }
                 }
             }
